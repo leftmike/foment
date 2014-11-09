8b57c99c537cb1bd79c7dc80e277ada4944824aa
Michael Montague <mikemon@gmail.com>
2014-11-09 10:04:28 -0800

    more cnstpass

--- src/foment.hpp
+++ src/foment.hpp
@@ -30,7 +30,7 @@ Compiler:
 -- genpass: LetrecStarValues needs to be compiled correctly
 -- export letrec-values and letrec*-values from (foment base)
 -- inlpass: inline lambdas
--- make Middle Four part of genpass or analpass
+-- make Middle Four part of genpass or anapass
 -- pass all non-local references into each lambda: frames can go away
 -- get rid of Level on FLambda
 -- treat case-lambda like lambda and have an entry point to compile and to each pass
