44d54e6a1e531c611664edff493217cdc79058c2
Michael Montague <mikemon@gmail.com>
2015-02-14 06:54:12 -0800

    profiling and use builtins for population count

--- src/hashmap.cpp
+++ src/hashmap.cpp
@@ -15,6 +15,16 @@ const uint64_t m2  = 0x3333333333333333; //binary: 00110011..
 const uint64_t m4  = 0x0f0f0f0f0f0f0f0f; //binary:  4 zeros,  4 ones ...
 const uint64_t h01 = 0x0101010101010101; //the sum of 256 to the power of 0,1,2,3...
 
+#ifdef FOMENT_UNIX
+#ifdef FOMENT_64BIT
+#define PopulationCount(x) __builtin_popcountl(x)
+#endif
+#ifdef FOMENT_32BIT
+#define PopulationCount(x) __builtin_popcount(x)
+#endif
+#endif // FOMENT_UNIX
+
+#ifndef PopulationCount
 int PopulationCount(uint64_t x)
 {
     x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
@@ -22,6 +32,7 @@ int PopulationCount(uint64_t x)
     x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits
     return (x * h01)>>56;  //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ...
 }
+#endif
 
 // ---- Hash Tree ----
 //
